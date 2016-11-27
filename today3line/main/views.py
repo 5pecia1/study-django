f95e995d4f2b0aac625a525868af3624fa8c33bc
5pecia1 <anmenmolsu22@naver.com>
2016-11-27 18:27:19 +0900

    add parsecast.py add data

--- today3line/main/views.py
+++ today3line/main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(reg) :
+    return render(reg, 'main/index.html') # render는 자신의 경로에서 templates이라는 폴더를 찾는다.
